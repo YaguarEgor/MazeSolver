724a7fdcc149bce4272f0c27a04d2ce5929b5af0
YaguarEgor <Egor072005@yandex.ru>
2024-10-23 23:42:24 +0300

    Window is made

---
+++ Window.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.__root_widget = Tk()
+        self.__root_widget.title = "Maze Solver"
+        self.__canvas = Canvas()
+        self.__canvas.pack()
+        self.__is_running = False
+        self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self.__root_widget.update()
+        self.__root_widget.update_idletasks()
+        
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+    
+    def close(self):
+        self.__running = False
+        
+        
